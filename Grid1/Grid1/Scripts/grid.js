c7c5dbfbd307ec8b4d3afeeaecf24daab92ff581
Gabriel Henrique <gabriel.holiveira@live.com>
2019-06-01 17:21:27 -0300

    first commit

---
+++ Grid1/Grid1/Scripts/grid.js
@@ -0,0 +1,48 @@
+﻿function Grid(reponse, type) {
+    debugger
+    const array = [];
+
+    const result = TypeGrid(type);
+
+    const div = document.createElement('div');
+    const table = document.createElement('table');
+    const trH = document.createElement('tr')
+
+    document.body.appendChild(div);
+    div.appendChild(table);
+    table.appendChild(trH);
+
+    for (let key in result) {
+        const th = document.createElement('th');
+        trH.appendChild(th);
+        th.appendChild(document.createTextNode(result[key]));
+        array.push(key);
+    }
+
+    for (let index = 0; index < reponse.length; index++) {
+        const trD = document.createElement('tr');
+        table.appendChild(trD);
+
+        for (let i = 0; i < array.length; i++) {
+            const td = document.createElement('td');
+            trD.appendChild(td);
+            td.appendChild(document.createTextNode(reponse[index][array[i]]));
+        }
+    }
+
+}
+
+function TypeGrid(type) {
+    switch (type) {
+        case 'TrabalhadorGerModel':
+            return {
+                'codigo': 'Código',
+                'nome': 'Nome',
+                'descricaoEmpresa': 'Empresa',
+                'descricaoCargo': 'Cargo'
+            };
+
+        default:
+            break;
+    }
+}
---
+++ Grid1/Grid1/Views/Home/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using System.Web.UI;
+
+namespace Grid1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public class TrabalhadorGerModel
+        {
+            public int codigo { get; set; }
+            public string nome { get; set; }
+            public int empresa { get; set; }
+            public string descricaoEmpresa { get; set; }
+            public int registro { get; set; }
+            public string descricaoCargo { get; set; }
+        }
+
+        public ActionResult Index()
+        {
+            TrabalhadorGerModel obj1 = new TrabalhadorGerModel()
+            {
+                codigo = 1,
+                nome = "gabriel",
+                empresa = 001,
+                descricaoEmpresa = "datamace",
+                registro = 1,
+                descricaoCargo = "analista programador jr"
+            };
+
+            TrabalhadorGerModel obj2 = new TrabalhadorGerModel()
+            {
+                codigo = 2,
+                nome = "lucas",
+                empresa = 001,
+                descricaoEmpresa = "datamace",
+                registro = 2,
+                descricaoCargo = "analista programador jr"
+            };
+
+            IList<TrabalhadorGerModel> list = new List<TrabalhadorGerModel>();
+            list.Add(obj1);
+            list.Add(obj2);
+
+            JavaScriptSerializer oSerializer = new JavaScriptSerializer();
+            string sJSON = oSerializer.Serialize(list);
+
+            ViewBag.Data = sJSON;
+            ViewBag.Type = "TrabalhadorGerModel";
+
+            return View();
+        }
+    }
+}
